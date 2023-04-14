24a565fa6d8035bd5bcd911bc6451184104e4dad
Tushar <tm2582001@gmail.com>
2023-04-14 19:50:22 +0530

    changed loading ui

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/pokedex/"
-})
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
+  base: "/pokedex/",
+});
